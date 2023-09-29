ef03d4167a3fe0d40da6e6a784585aaf4d5b595b
noguchi <yasu2704.kuro@gmail.com>
2023-09-29 10:05:34 +0000

    fix fetch

--- app/now.ts
+++ app/now.ts
@@ -4,9 +4,7 @@ export async function getNow() {
   const headersList = headers();
   const host = headersList.get("host");
   const protocol = headersList.get("x-forwarded-proto") || "http";
-  const res = await fetch(new URL("/api/time", `${protocol}://${host}`).href, {
-    cache: "force-cache",
-  });
+  const res = await fetch(new URL("/api/time", `${protocol}://${host}`).href);
   const { now } = await res.json();
   return now;
 }
